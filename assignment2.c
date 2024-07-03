3a69209444e139c79b1b54f80edade921897d30b
ishan-053 <shindeishan042@gmail.com>
2024-07-03 09:19:35 +0530

    Assignment2

---
+++ assignment2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int line, r, c;
+
+    line=5;
+    
+
+    r = 1;
+    while (r <= line) {
+        c = 1;
+        
+        
+        while (c <= line - r) {
+            printf(" ");
+            c++;
+        }
+
+        c = 1;
+        
+        while (c <= 2 * r - 1) {
+            printf("*");
+            c++;
+        }
+        printf("\n");
+
+        
+        r++;
+    }
+
+    return 0;
+}
+
