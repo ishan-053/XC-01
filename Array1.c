7740a5c5b60572d1753de0e2cafdfa34ac1c60a8
Taksheel6669 <taksheels6669@gmail.com>
2024-07-11 09:56:20 +0530

    Update Array1.c

--- Array1.c
+++ Array1.c
@@ -4,28 +4,28 @@
    #include<stdio.h>
    void main()
    {
-   	int   x[5],no;
+   	int   x[5],no,i;
    	int   add_1,add_2,add_3,add_4,add_5;
 
-	   printf("\n Enter 5 Integer Number"):
+	   printf("\n Enter 5 Integer Number");
            for(i=0;i<=4;i++)
 		   {
 			   scanf("%d",&no);
 		   }
    	
-   	add_1 = (x[0]/100)+(x[0]/10)%10+(x[0]%10);
+   	add_1 = (x[i]/100)+(x[i]/10)%10+(x[i]%10);
    	printf("\n Addn of Single Digit for 1st Value : %d",add_1);
    	
-   	add_2 = (x[1]/100)+(x[1]/10)%10+(x[1]%10);
+   	add_2 = (x[i]/100)+(x[i]/10)%10+(x[i]%10);
    	printf("\n Addn of Single Digit for 2nd Value : %d",add_2);
    	
-   	add_3 = (x[2]/100)+(x[2]/10)%10+(x[2]%10);
+   	add_3 = (x[i]/100)+(x[i]/10)%10+(x[i]%10);
    	printf("\n Addn of Single Digit for 3rd Value : %d",add_3);
    	
-   	add_4 = (x[2]/100)+(x[2]/10)%10+(x[2]%10);
+   	add_4 = (x[i]/100)+(x[i]/10)%10+(x[i]%10);
    	printf("\n Addn of Single Digit for 4th Value : %d",add_4);
    	
-   	add_5 = (x[2]/100)+(x[2]/10)%10+(x[2]%10);
+   	add_5 = (x[i]/100)+(x[i]/10)%10+(x[i]%10);
    	printf("\n Addn of Single Digit for 5th Value : %d",add_5);
    	
    	if(add_1>10)
